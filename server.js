39194d49e0b09179b00c34791b32b8c9c473665b
Nataliia Mogylchuk <nataliia.mogylchuk@gmail.com>
2020-06-02 23:31:17 +0300

    Add heroku config

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const path = require('path');
 const app = express();
-
-app.use(express.static(__dirname + 'dist/<angular-tvmaze>'));
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/<angular-tvmaze>/index.html'));
-});
+const path = require('path');
+ 
+app.use(express.static(__dirname + '/dist/angular-tvmaze'));
 
 app.listen(process.env.PORT || 8080);
+ 
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/angular-tvmaze/index.html'));
+});
+
+console.log('Console listening!');
